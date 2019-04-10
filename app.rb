eb4cd17f08b59b786fc575868fc29d034775f4f0
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-10 19:36:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # create get route in '/' to display the form user_input
+  # erb :user_input
+
+  # create post route in /piglatinize to create an instance of piglatinizer and
+  # @piglatinize = PigLatinizer.new(params[:user_phrase])
+  #
+  # display the results of the piglatinization
+  # erb :piglatinized
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,15 @@
+class PigLatinizer
+  attr_reader :text
+
+  # def initialize(text)
+  #   @text = text
+  # end
+
+  # check for consonants
+
+  #method to transform word
+
+  # def transformed_text 
+  #method loop split the text in words, map through the text and call the transform word
+  # to each word
+end
