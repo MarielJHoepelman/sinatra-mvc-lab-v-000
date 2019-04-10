663a14fa234595354532b4c3bef11dfd9319907f
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-10 20:00:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,23 @@
 class PigLatinizer
   attr_reader :words
 
+  def is_consonant?(letter)
+    (/[aeiou]/i).match(letter).nil?
+  end
+  
+  def piglatinize(word)
+    transformed = ''
+    if !is_consonant?(word[0])
+      transformed = word + "way"
+    elsif is_consonant?(word[0]) && is_consonant?(word[1]) && is_consonant?(word[2])
+      transformed = word.slice(3..-1) + word.slice(0..2) + 'ay'
+    elsif is_consonant?(word[0]) && is_consonant?(word[1])
+      transformed = word.slice(2..-1) + word.slice(0..1) + 'ay'
+    else
+      transformed = word.slice(1..-1) + word.slice(0..0) + 'ay'
+    end
+    transformed
+  end
   # check for consonants
 
   #method to transform word
