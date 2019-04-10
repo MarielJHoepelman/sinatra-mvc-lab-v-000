3192093d0c623a70cd0e5193b09335487948135c
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-10 19:53:54 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,15 @@
 class PigLatinizer
   attr_reader :text
 
-  # def initialize(text)
-  #   @text = text
-  # end
+  def initialize(text)
+    @text = text
+  end
 
   # check for consonants
 
   #method to transform word
 
-  # def transformed_text 
+  # def transformed_text
   #method loop split the text in words, map through the text and call the transform word
   # to each word
 end
